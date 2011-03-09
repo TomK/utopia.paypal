67304af66de0e6115432f3a33e9141f66e71b8fb
Tom Kay <oridan82@gmail.com>
2011-03-09 15:50:32 +0000

    fix bug with script and container not being included

--- paypal.php
+++ paypal.php
@@ -103,14 +103,14 @@ class PayPal extends uBasicModule {
 	private static $cvvAdded = false;
 	public static function DrawCVVField($v) {
 		if (!self::$cvvAdded) {
-			utopia::AppendVar('content','<div id="cvv2info" style="display:none;text-align:center;"><img src="'.utopia::GetRelativePath(dirname(__FILE__).'/cvv2.jpg').'" alt="" /></div>');
-			utopia::AppendVar('script_include', 'function openCVV() { $(\'#cvv2info\').dialog({autoOpen:false,modal:true,width:550,buttons:{"Close":function() { $(this).dialog("close"); }},position:["center",100],resizable:false,draggable:false,title:"Where do i find my CVV2?"}); $(\'#cvv2info\').dialog("open"); }');
+			self::$cvvAdded = true;
+			echo '<div id="cvv2info" style="display:none;text-align:center;"><img src="'.utopia::GetRelativePath(dirname(__FILE__).'/cvv2.jpg').'" alt="" /></div>';
+			uJavascript::AddText('function openCVV() { $(\'#cvv2info\').dialog({autoOpen:false,modal:true,width:550,buttons:{"Close":function() { $(this).dialog("close"); }},position:["center",100],resizable:false,draggable:false,title:"Where do i find my CVV2?"}); $(\'#cvv2info\').dialog("open"); }');
 		}
 		return '<a style="text-decoration:underline" href="javascript:openCVV()">CVV2?</a>';
 	}
 	public static function AddCVVField($module) {
 		CallModuleFunc($module, 'AddField', '__cvv2__',array('PayPal','DrawCVVField'),'','');
-		self::$cvvAdded = true;
 	}
 
 	/**
